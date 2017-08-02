af00ff6a268dddc2cace4c6c9f3e5ef1fe4bbf12
Claudio Botelho <claudio.botelho@smartificar.com.br>
2017-08-02 17:51:30 -0300

    solution exercise#1 day#28 reviews screen fixed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,20 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant
+
   def index
-    @reviews = Review.all
+    @reviews = @restaurant.reviews
   end
 
   def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
     @review = Review.new(review_params)
-    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    @review.save # Recall what ! does it here after save
     if @review.save
-      redirect_to restaurant_path(@review)
+      redirect_to restaurant_path(@restaurant)
     else
       render :new
     end
@@ -22,5 +24,9 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:content, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
